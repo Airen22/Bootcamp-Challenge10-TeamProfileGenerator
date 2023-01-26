ca27e106b88c3b0645dfe931ba01fc3fbe3935cd
Erin <erinpeifer@gmail.com>
2023-01-25 21:21:01 -0600

    added getters

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer');
+const jest = require('jest');
+const fs = require('fs');
+const employee = require('employee')
+
+
+// const generateManager = ({}) =>
+// `HTML manager container`;
+
+// const generateEngineer = ({}) =>
+// `HTML engineer contaner`;
+
+// const generateIntern = ({}) =>
+// `HTML intern container`;
+
+// const generateHTML = ({}) =>
+// `HTML base frame with ${generateManager}, ${Engineer}, ${Intern}
