0797c514e2faed2363ecec98a97ee8bb9609b652
Erin <erinpeifer@gmail.com>
2023-01-26 00:22:24 -0600

    prompts for each role work when node index.js is called in terminal; also added generateHTML into template.js

--- index.js
+++ index.js
@@ -1,9 +1,150 @@
 const inquirer = require('inquirer');
 const jest = require('jest');
 const fs = require('fs');
-const employee = require('employee')
+// const employee = require('Employee');
+// const manager = require('Manager');
+// const engineer = require('Engineer');
+// const intern = require('Intern')
 
 
+const confirmInput = (input) => {
+    if (input == "") {
+        return `Did you mean to leave this blank? Please enter N/A if there is no content for this section.`
+    } return true
+};
+
+const createTeam = (input) => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Manager Name:',
+            name: 'name',
+            validate: confirmInput
+        },
+        {
+            type: 'input',
+            message: 'Manager ID:',
+            name: 'id',
+            validate: confirmInput
+        },
+        {
+            type: 'input',
+            message: 'Manager email:',
+            name: 'email',
+            validate: confirmInput
+        },
+        {
+            type: 'input',
+            message: 'Manager office number:',
+            name: 'officeNumber',
+            validate: confirmInput
+        },
+        {
+            type: 'list',
+            message: 'Would you like to add a new team member?',
+            name: 'newEmployee',
+            choices: ['Engineer', 'Intern', 'No additional employees'],
+        },
+    ])
+    .then((input) => {
+        if (input.newEmployee == 'Engineer') {
+            createEngineer(input)
+        } else if (input.newEmployee == 'Intern') {
+            createIntern(input)
+        } else if (input.newEmployee == 'No additonal employees') {
+            generateHTML(input)
+        }
+    })
+}
+
+function createEngineer (input) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Engineer Name:',
+            name: 'name',
+            validate: confirmInput
+        },
+        {
+            type: 'input',
+            message: 'Engineer ID:',
+            name: 'id',
+            validate: confirmInput
+        },
+        {
+            type: 'input',
+            message: 'Engineer email:',
+            name: 'email',
+            validate: confirmInput
+        },
+        {
+            type: 'input',
+            message: 'Engineer GitHub:',
+            name: 'github',
+            validate: confirmInput
+        },
+        {
+            type: 'list',
+            message: 'Would you like to add a new team member?',
+            name: 'newEmployee',
+            choices: ['Engineer', 'Intern', 'No additional employees'],
+        },
+    ]    )
+    .then((input) => {
+        if (input.newEmployee == 'Engineer') {
+            createEngineer(input)
+        } else if (input.newEmployee == 'Intern') {
+            createIntern(input)
+        } else if (input.newEmployee == 'No additonal employees') {
+            generateHTML(input)
+        }
+    })
+}
+
+function createIntern (input) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Intern Name:',
+            name: 'name',
+            validate: confirmInput
+        },
+        {
+            type: 'input',
+            message: 'Intern ID:',
+            name: 'id',
+            validate: confirmInput
+        },
+        {
+            type: 'input',
+            message: 'Intern email:',
+            name: 'email',
+            validate: confirmInput
+        },
+        {
+            type: 'input',
+            message: 'Intern school:',
+            name: 'school',
+            validate: confirmInput
+        },
+        {
+            type: 'list',
+            message: 'Would you like to add a new team member?',
+            name: 'newEmployee',
+            choices: ['Engineer', 'Intern', 'No additional employees'],
+        },
+    ]    )
+    .then((input) => {
+        if (input.newEmployee == 'Engineer') {
+            createEngineer(input)
+        } else if (input.newEmployee == 'Intern') {
+            createIntern(input)
+        } else if (input.newEmployee == 'No additonal employees') {
+            generateHTML(input)
+        }
+    })
+}
+createTeam()
 // const generateManager = ({}) =>
 // `HTML manager container`;
 
