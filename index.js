afab04bfeb00307bd6dd6566ed145b5f40981d29
Erin <erinpeifer@gmail.com>
2023-01-26 21:03:49 -0600

    prompts are consolidated, new constructors get greated and added to employee array

--- index.js
+++ index.js
@@ -1,88 +1,104 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
 const jest = require('jest');
-const fs = require('fs');
-const generateHTML = require('./src/template')
+const generateHTML = require('./src/template');
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern')
-
-
+const Intern = require('./lib/Intern');
+const teamArr = [];
+const newRole = "";
+const newName = "";
+const newId = "";
+const newEmail = "";
 const confirmInput = (input) => {
     if (input == "") {
         return `Did you mean to leave this blank? Please enter N/A if there is no content for this section.`
     } return true
 };
 
-const createTeam = (input) => {
+const createTeam = () => {
     inquirer.prompt([
         {
+            type: 'list',
+            message: 'Select team member role?',
+            name: 'role',
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },])
+        .then((input) => {
+            let newRole = input.role
+            inquirer.prompt([
+        {
             type: 'input',
-            message: 'Manager Name:',
+            message: `${newRole}'s name:`,
             name: 'name',
             validate: confirmInput
         },
         {
             type: 'input',
-            message: 'Manager ID:',
+            message: `${newRole}'s ID:`,
             name: 'id',
             validate: confirmInput
         },
         {
             type: 'input',
-            message: 'Manager email:',
+            message: `${newRole}'s email:`,
             name: 'email',
             validate: confirmInput
         },
+    ])
+    .then(function (input) {
+        let newName = input.name;
+        let newId = input.id;
+        let newEmail = input.email;
+        if (newRole == 'Manager') {
+            createManager(newName, newId, newEmail, newRole)
+        } else if (newRole == 'Engineer') {
+            createEngineer(newName, newId, newEmail, newRole)
+        } else if (newRole == 'Intern') {
+            createIntern(newName, newId, newEmail, newRole)
+        }
+        
+        
+        // (input.newEmployee == 'No additional employees') {
+        //     console.log(input);
+        //     generateHTML(input)
+        // }
+    
+
+
+
+function createManager (newName, newId, newEmail) {
+    inquirer.prompt([
         {
             type: 'input',
-            message: 'Manager office number:',
+            message: `Manager's office number:`,
             name: 'officeNumber',
             validate: confirmInput
-        },
+        },        
         {
             type: 'list',
             message: 'Would you like to add a new team member?',
             name: 'newEmployee',
-            choices: ['Engineer', 'Intern', 'No additional employees'],
+            choices: ['Yes', 'No'],
         },
     ])
     .then((input) => {
-        const manager = new Manager(input.name, input.id, input.email, input.officeNumber)
-        if (input.newEmployee == 'Engineer') {
-            createEngineer(input)
-        } else if (input.newEmployee == 'Intern') {
-            createIntern(input)
-        } else if (input.newEmployee == 'No additional employees') {
-            console.log(input);
-            generateHTML(input)
+    const newMember = new Manager(newName, newId, newEmail, input.officeNumber)
+    teamArr.push(newMember)
+        if (input.newEmployee == 'No') {
+            console.log(teamArr)
+            // generateHTML(input)
+        } else {
+            createTeam();
         }
     })
 }
-
-function createEngineer (input) {
+function createEngineer (newName, newId, newEmail) {
     inquirer.prompt([
         {
             type: 'input',
-            message: 'Engineer Name:',
-            name: 'name',
-            validate: confirmInput
-        },
-        {
-            type: 'input',
-            message: 'Engineer ID:',
-            name: 'id',
-            validate: confirmInput
-        },
-        {
-            type: 'input',
-            message: 'Engineer email:',
-            name: 'email',
-            validate: confirmInput
-        },
-        {
-            type: 'input',
-            message: 'Engineer GitHub:',
+            message: `Engineer's GitHub:`,
             name: 'github',
             validate: confirmInput
         },
@@ -90,44 +106,26 @@ function createEngineer (input) {
             type: 'list',
             message: 'Would you like to add a new team member?',
             name: 'newEmployee',
-            choices: ['Engineer', 'Intern', 'No additional employees'],
+            choices: ['Yes', 'No'],
         },
     ]    )
-    .then((input) => {
-        if (input.newEmployee == 'Engineer') {
-            createEngineer(input)
-        } else if (input.newEmployee == 'Intern') {
-            createIntern(input)
-        } else if (input.newEmployee == 'No additonal employees') {
-            console.log(response);
-            generateHTML(input)
+   .then((input) => {
+    const newMember = new Engineer(newName, newId, newEmail, input.github)
+    teamArr.push(newMember)
+        if (input.newEmployee == 'No') {
+            console.log(teamArr)
+            // generateHTML(input)
+        } else {
+            createTeam();
         }
     })
 }
 
-function createIntern (input) {
+function createIntern (newName, newId, newEmail) {
     inquirer.prompt([
         {
             type: 'input',
-            message: 'Intern Name:',
-            name: 'name',
-            validate: confirmInput
-        },
-        {
-            type: 'input',
-            message: 'Intern ID:',
-            name: 'id',
-            validate: confirmInput
-        },
-        {
-            type: 'input',
-            message: 'Intern email:',
-            name: 'email',
-            validate: confirmInput
-        },
-        {
-            type: 'input',
-            message: 'Intern school:',
+            message: `Intern's school:`,
             name: 'school',
             validate: confirmInput
         },
@@ -135,19 +133,22 @@ function createIntern (input) {
             type: 'list',
             message: 'Would you like to add a new team member?',
             name: 'newEmployee',
-            choices: ['Engineer', 'Intern', 'No additional employees'],
+            choices: ['Yes', 'No'],
         },
     ]    )
-    .then((input) => {
-        if (input.newEmployee == 'Engineer') {
-            createEngineer(input)
-        } else if (input.newEmployee == 'Intern') {
-            createIntern(input)
-        } else if (input.newEmployee == 'No additonal employees') {
-            console.log(response);
-            generateHTML(input)
+   .then((input) => {
+    const newMember = new Intern(newName, newId, newEmail, input.school)
+    teamArr.push(newMember)
+        if (input.newEmployee == 'No') {
+            console.log(teamArr)
+            // generateHTML(input)
+        } else {
+            createTeam();
         }
     })
+}
+})
+})
 }
 createTeam()
 // const generateManager = ({}) =>
